90290a8e58fada43e595dba80b9063d23e44959d
陳棨煜 <tony32059613@gmail.com>
2023-05-21 18:54:58 +0800

    完成基本UI界面及功能

---
+++ content.js
@@ -0,0 +1,68 @@
+chrome.runtime.onMessage.addListener(
+    function (message, sender, sendResponse) {
+        if (message.type === "task1") {
+            showUI("基於以下文章，以繁體中文出五題選擇題，每題四個選項，題目類型包括文意、修辭等範圍，並附上答案");
+        }
+        if (message.type === "task2") {
+            showUI("依據以下計畫徵件說明撰寫章節大綱");
+        }
+        if (message.type === "task3") {
+            showUI("依據以下計畫章節與相關資料撰寫計畫章節內文");
+        }
+        if (message.type === "task4") {
+            showUI("依據以下資料，思考並架構在教學時，老師可能會遭遇的問題，請列表格寫出師生困境");
+        }
+    }
+);
+
+const taskUI = document.createElement('div');
+const row1 = document.createElement('div');
+const taskText = document.createElement('div');
+const input = document.createElement('textarea');
+const row2 = document.createElement('div');
+const sendBtn = document.createElement('button');
+const closeBtn = document.createElement('button');
+const bg = document.createElement("div");
+
+function initUI(){
+    taskUI.classList.add('task-ui');
+    row1.classList.add('row1');
+    taskText.classList.add('task-text');
+    // input.setAttribute('type', 'text');
+    row2.classList.add('row2');
+
+    sendBtn.classList.add('send-btn');
+    sendBtn.innerText = "Send";
+    sendBtn.addEventListener("click", async function(){
+        
+    });
+
+    closeBtn.classList.add('close-btn');
+    closeBtn.innerText = "Close";
+    closeBtn.addEventListener("click", async function(){
+        removeUI();
+    });
+
+    bg.classList.add("alert-bg");
+
+    row1.appendChild(taskText);
+    row1.appendChild(input);
+    taskUI.appendChild(row1);
+    row2.appendChild(sendBtn);
+    row2.appendChild(closeBtn);
+    taskUI.appendChild(row2);
+}
+
+initUI();
+
+function showUI(prompt){
+    taskText.innerText = prompt;
+    document.body.appendChild(taskUI);
+    document.body.appendChild(bg);
+}
+
+function removeUI(){
+    taskUI.remove();
+    bg.remove();
+}
+
--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("open-btn").addEventListener("click", function () {
+    document.getElementById("task1-btn").addEventListener("click", function () {
         chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
             var activeTab = tabs[0];
-            chrome.tabs.sendMessage(activeTab.id, {type: "open"});
+            chrome.tabs.sendMessage(activeTab.id, {type: "task1"});
+        });
+    });
+    document.getElementById("task2-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {type: "task2"});
+        });
+    });
+    document.getElementById("task3-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {type: "task3"});
+        });
+    });
+    document.getElementById("task4-btn").addEventListener("click", function () {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, {type: "task4"});
         });
     });
 });
