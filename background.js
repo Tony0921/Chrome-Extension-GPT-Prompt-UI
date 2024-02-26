fd14f2da9679051cf4e9a9356f952a101234ee94
陳棨煜 <tony32059613@gmail.com>
2024-02-26 18:19:45 +0800

    1.新增引導介面 2.主介面優化、加入icon 3.更新版本號

---
+++ background.js
@@ -0,0 +1,21 @@
+function getTabInfo() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        if (activeTab.url.includes("chat.openai.com")) {
+            console.log("match");
+            chrome.action.setPopup({ popup: './popup/popup.html' });
+        } else {
+            chrome.action.setPopup({ popup: './popup/popup2.html' });
+        }
+    });
+}
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    getTabInfo();
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.active && changeInfo.url) {
+        getTabInfo();
+    }
+});
